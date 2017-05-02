7e7c24de3a1e3b0e39b325491904b4e1c419ee22
Thomas Wiringa <thomas.wiringa@gmail.com>
2017-05-02 16:52:29 +0200

    Handle errors from the spotify API cleaner

--- app/Http/Controllers/PlayingController.php
+++ app/Http/Controllers/PlayingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
 
 /**
  * Playing controller
@@ -11,30 +12,51 @@ use GuzzleHttp\Client;
  */
 class PlayingController extends Controller
 {
+    /**
+     * Playing page
+     *
+     * @return \Illuminate\View\View
+     */
     public function handle()
     {
         return view('playing');
     }
 
+    /**
+     * Get the data from Spotify
+     *
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
+     */
     public function fetch()
-    { // curl -X GET "https://api.spotify.com/v1/me/player" -H "Authorization: Bearer {your access token}"
-        if (!session()->has('access_token')) {
+    {
+        if (!session()->has('access_token') || !request()->ajax()) {
             return redirect('/');
         }
 
         $client = new Client;
-        $response = $client->get("https://api.spotify.com/v1/me/player/currently-playing", [
-            'headers' => [
-                'Authorization' => 'Bearer ' . session('access_token')
-            ]
-        ]);
+        try {
+            $response = $client->get("https://api.spotify.com/v1/me/player/currently-playing", [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . session('access_token')
+                ]
+            ]);
 
-        $code = $response->getStatusCode();
+            $refresh = false;
+            $code = $response->getStatusCode();
+            $payload = $response->getBody()->getContents();
+        } catch (BadResponseException $e) {
+            $refresh = false;
+            $code = $e->getResponse()->getStatusCode();
+            $payload = [
+                "message" => "Loading from Spotify API failed.",
+            ];
+        }
 
         return response([
             'code' => $code,
             'success' => $code >= 200 && $code < 300,
-            'payload' => $response->getBody()->getContents()
+            'refresh' => $refresh,
+            'payload' => $payload
         ]);
     }
 }
